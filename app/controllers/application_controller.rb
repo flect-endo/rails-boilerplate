5b8a73a90075a781bb054ea7cc634869c759b39e
flect-endo <takumi.endo@flect.co.jp>
2015-03-04 18:38:25 +0900

    認証トークンによるユーザ認証ロジックを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,20 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  skip_before_action :verify_authentication_token, if: -> { request.format.json? }
+  before_action :authenticate_user_from_token!, if: -> { params[:email].present? }
+
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to home_index_path, alert: exception.message
+    respond_to do |format|
+      format.html { redirect_to home_index_path, alert: exception.message }
+      format.json { render json: { message: exception.message }, status: :unauthorized }
+    end
+  end
+
+  def authenticate_user_from_token!
+    user = User.where(email: params[:email]).first
+    if Devise.secure_compare(user.try(:authentication_token), params[:token])
+      sign_in user, store: false
+    end
   end
 end
