d57d50a3f3defe1081a7c64b7c5c465f19a4a990
flect-endo <takumi.endo@flect.co.jp>
2015-03-05 12:41:43 +0900

    クロスドメイン対策でGemを使うようにした

--- config/application.rb
+++ config/application.rb
@@ -30,11 +30,16 @@ module RailsBoilerplate
     end
 
     # Cross Domain 対応
-    config.action_dispatch.default_headers = {
-      'Access-Control-Allow-Credentials' => 'true',
-      # FIXME: ローカルの Ionic からしか受け付けていないので修正
-      'Access-Control-Allow-Origin' => 'http://localhost:8100',
-      'Access-Control-Request-Method' => '*'
-    }
+    # FIXME: ローカルの Ionic からしか受け付けていないので要検討
+    config.middleware.insert_before Warden::Manager, Rack::Cors do
+      allow do
+        origins 'localhost:8100'
+        resource '*',
+          headers: :any,
+          methods: [:get, :post, :delete, :put, :options],
+          credentials: true,
+          max_age: 86400
+      end
+    end
   end
 end
