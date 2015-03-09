85a716753c949976ab92c1630ab3f029413bbf88
flect-endo <takumi.endo@flect.co.jp>
2015-03-09 16:39:36 +0900

    debug1@

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -20,7 +20,10 @@ class HomeController < ApplicationController
     secret = Rails.application.secrets.client_secret
     # session[:signed_request] =
     decoded = Restforce.decode_signed_request(params[:signed_request], secret)
+    logger.info "*********************************1"
     logger.info decoded
+    logger.info "*********************************2"
+    logger.info decoded.length
     redirect_to home_index_url
   end
 end
