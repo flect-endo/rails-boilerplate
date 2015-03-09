6cdcc10212d073d2396ca2ab51347c08b627b33f
flect-endo <takumi.endo@flect.co.jp>
2015-03-09 11:48:01 +0900

    debug

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
 
   def index
+    logger.info "-------------------------------------------------"
+    request.headers.each {|k, v| logger.info k}
   end
 end
