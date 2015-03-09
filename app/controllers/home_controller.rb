5f74e42f2e4a2f93c5cb04527d486d25df6e84cb
flect-endo <takumi.endo@flect.co.jp>
2015-03-09 16:35:41 +0900

    debug CookieOverflow

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -18,7 +18,9 @@ class HomeController < ApplicationController
     logger.info params
 
     secret = Rails.application.secrets.client_secret
-    session[:signed_request] = Restforce.decode_signed_request(params[:signed_request], secret)
+    # session[:signed_request] =
+    decoded = Restforce.decode_signed_request(params[:signed_request], secret)
+    logger.info decoded
     redirect_to home_index_url
   end
 end
