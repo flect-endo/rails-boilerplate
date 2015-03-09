ada570f77a7edd3d0e38043772b285508b18b092
flect-endo <takumi.endo@flect.co.jp>
2015-03-09 16:09:42 +0900

    CSRF対応

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
   # before_action :authenticate_user!
 
+  protect_from_forgery except: :index_auth
+
   def index
     logger.info "request headers -------------------------------------------------"
     request.headers.each {|k, v| logger.info k}
@@ -16,7 +18,13 @@ class HomeController < ApplicationController
     logger.info params
 
     secret = Rails.application.secrets.client_secret
-    session[:signed_request] = Restforce.decode_signed_request(params[:signed_request], secret)
-    redirect '/home/index'
+    # session[:signed_request] =
+    decoded = Restforce.decode_signed_request(params[:signed_request], secret)
+    logger.info "*********************************1"
+    logger.info decoded
+    logger.info "*********************************2"
+    logger.info decoded.length
+    session[:signed_request] = decoded
+    redirect_to home_index_url
   end
 end
