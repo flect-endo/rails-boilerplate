5354b8645126aa71b4a8319e3c2e618ebd424440
flect-endo <takumi.endo@flect.co.jp>
2015-03-11 15:54:02 +0900

    #6 Salesforceアクセス結果を画面から確認できるようにした

--- app/controllers/salesforce_controller.rb
+++ app/controllers/salesforce_controller.rb
@@ -1,10 +1,33 @@
+require 'restforce'
+
 class SalesforceController < ApplicationController
   before_action :authenticate_user!
+  before_action :build_client, only: [:index]
 
   def index
+    @accounts = @client.query("select Id, Name, (select Name from Contacts) from Account")
   end
 
   def credentials
     render json: (session[:credentials] || {}).to_json
+  end
+
+  def onload
+    @success, @error = params[:success], params[:error]
+  end
+
+  private
+
+  def build_client
+    # TODO: セッションに認証トークンがない場合(=通常フォームでのログイン)、
+    # 本当は例外を投げるようにしたほうがよい
+    credentials = session[:credentials] || {}
+    @client = Restforce.new(
+      oauth_token: credentials[:oauth_token],
+      refresh_token: credentials[:refresh_token],
+      instance_url: credentials[:instance_url],
+      client_id: Rails.application.secrets.client_key,
+      client_secret: Rails.application.secrets.client_secret
+    )
   end
 end
