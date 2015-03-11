e930e9eba62181a275830038294d1602942e00c1
flect-endo <takumi.endo@flect.co.jp>
2015-03-11 19:02:31 +0900

    現パスワードなしでアカウント変更できるようにした

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,12 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 
+  protected
+
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
+
   def account_update_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
