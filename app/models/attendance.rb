5afc21d0edcd7ef58f384e09c3d5df7436f06193
flect-endo <takumi.endo@flect.co.jp>
2015-03-17 09:45:59 +0900

    デフォルト引数を追加して、勤怠対象日などを指定できるようにした

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,16 +1,16 @@
 class Attendance < ActiveRecord::Base
   belongs_to :user
 
-  def self.clock_in(user)
-    where(user: user, date: Date.today).first \
-    || create!(user: user, date: Date.today, started_at: Time.current)
+  def self.clock_in(user, date=Date.today, started_at=Time.current)
+    where(user: user, date: date).first \
+    || create!(user: user, date: date, started_at: started_at)
   end
 
-  def self.clock_out(user)
-    attendance = Attendance.where(user: user, date: Date.today).first
+  def self.clock_out(user, date=Date.today, ended_at=Time.current)
+    attendance = Attendance.where(user: user, date: date).first
     raise "not start" if attendance.nil?
 
-    attendance.update_attributes!(ended_at: Time.current)
+    attendance.update_attributes!(ended_at: ended_at)
     attendance
   end
 end
