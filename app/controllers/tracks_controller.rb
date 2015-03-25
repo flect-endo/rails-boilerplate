a5f7c87297f1d072301d602f554d2a7fd33bde59
flect-endo <takumi.endo@flect.co.jp>
2015-03-25 17:16:17 +0900

    トラックデータの削除機能を追加

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,10 +1,18 @@
 class TracksController < ApplicationController
+  before_action :set_track
 
   def load
-    track = Track.find(params[:id])
-    @trackpoints = JSON.parse(track.trackpoints)
+    @trackpoints = JSON.parse(@track.trackpoints)
   end
 
   def destroy
+    @track.destroy
+    @tracks = Track.all
+  end
+
+  private
+
+  def set_track
+    @track = Track.find(params[:id])
   end
 end
